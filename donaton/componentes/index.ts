a63e68334774458ed02634420e54e0b6c66591ee
LukMezac <luk.mezac@duocuc.cl>
2026-05-03 03:26:09 -0400

    refactor: centralizar exportaciones en el punto de entrada del paquete ui

--- donaton/componentes/index.ts
+++ donaton/componentes/index.ts
@@ -1,4 +1,4 @@
-// Archivo de exportación para el módulo NPM
-export { default as NavbarGlobal } from './NavbarGlobal';
-export { default as NavbarPerfil } from './NavbarPerfil';
-export { default as AlertaDocker } from './AlertaDocker';
+// Archivo: donaton/componentes/index.ts
+export * from './NavbarGlobal';
+export * from './NavbarPerfil';
+export * from './AlertaDocker';
