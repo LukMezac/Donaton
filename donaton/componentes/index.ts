091edad7b8266af0e896c65e2292ba57ee52bc5f
LukMezac <luk.mezac@duocuc.cl>
2026-05-02 23:54:48 -0400

    feat(ui): implementar empaquetado NPM y middleware de seguridad

---
+++ donaton/componentes/index.ts
@@ -0,0 +1,4 @@
+// Archivo de exportación para el módulo NPM
+export { default as NavbarGlobal } from './NavbarGlobal';
+export { default as NavbarPerfil } from './NavbarPerfil';
+export { default as AlertaDocker } from './AlertaDocker';
